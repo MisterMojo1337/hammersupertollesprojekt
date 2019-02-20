939551b56c4dc6b332a1af0fd6596aaa0b369871
trTribe <julian.schoepe@web.de>
2019-02-20 14:05:47 +0100

    Movement mit wasd und Controller dpad

---
+++ Assets/Scripts/Components/MovementComponent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody rb;
+
+    private bool moving;
+    private Vector2 direction;
+
+    private void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var move = new Vector3(direction.x, 0, direction.y) * Time.deltaTime * speed;
+        rb.MovePosition(rb.position + move);
+
+        if (moving)
+        {
+            
+        }
+    }
+
+    public void SetMoveInput(Vector2 dir)
+    {
+        moving = !(dir.x == 0 && dir.y == 0);
+        direction = dir;
+    }
+}
---
+++ Assets/Scripts/Components/Player/InputComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Input;
+
+public class InputComponent : MonoBehaviour
+{
+    [SerializeField] private InputMaster inputMaster;
+    [SerializeField] private MovementComponent movement;
+    
+    void Awake()
+    {
+        inputMaster.Player.Movement.performed += MovementInput;
+    }
+
+    void MovementInput(InputAction.CallbackContext context)
+    {
+        movement.SetMoveInput(context.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        inputMaster.Player.Movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputMaster.Player.Movement.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        inputMaster.Player.Movement.performed -= MovementInput;
+    }
+}
